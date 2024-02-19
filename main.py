d90f7c2a34f014d446eaeeff8aa9df6144bf1171
Netanel shvab <netanelshvab@gmail.com>
2024-02-19 23:16:23 +0200

    c

--- main.py
+++ main.py
@@ -6,12 +6,25 @@ def main(args=None):
     # Creating an instance of the Instaloader class
     bot = instaloader.Instaloader()
 
-    username = input("enter username")
-    password = input("enter password")
+    username = input("enter username:")
+    password = input("enter password:")
 
     # Log in to the Instagram account
-    profile = log_in_to_account(bot, username, password)
-    
+    didnt_pass_true = True
+    while didnt_pass_true:
+        try:
+            profile = log_in_to_account(bot, username, password)
+        except(instaloader.exceptions.ConnectionException):
+            print("wrong password, enter again:")
+            username = input()
+            didnt_pass_true = True
+        except(instaloader.exceptions.BadCredentialsException):
+            print("wrong password, enter again:")
+            username = input()
+            didnt_pass_true = True
+        else:
+            didnt_pass_true = False
+
     #get the main account followers and the followers followers info
     followers_info = get_followers_of_main_account(profile=profile)
     
@@ -19,7 +32,6 @@ def main(args=None):
     print("Followers:")
     for follower in followers_info:
         print(follower)
-
 
 
 if __name__ == "__main__":
